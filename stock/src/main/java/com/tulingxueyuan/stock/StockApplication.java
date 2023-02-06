e07d8f3d32c3695efab053730fcc78d9128a07fd
lyq <3471966565@qq.com>
2023-02-06 19:55:54 +0800

    Default Changelist

---
+++ stock/src/main/java/com/tulingxueyuan/stock/StockApplication.java
@@ -0,0 +1,16 @@
+package com.tulingxueyuan.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @author: 刘
+ * @date: 2023年02月06日 下午 4:29
+ */
+@SpringBootApplication
+public class StockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class,args);
+    }
+}
