c63cc4a3ff60066d7433914a82a233515a135556
lyq <3471966565@qq.com>
2023-02-08 00:20:01 +0800

    ~~~依赖修改2

---
+++ stock-nacos/src/main/java/com/tulingxueyuan/stock/StockApplication.java
@@ -0,0 +1,18 @@
+package com.tulingxueyuan.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description:
+ * @author: 刘
+ * @date: 2023年02月06日 下午 4:29
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class,args);
+    }
+}
---
+++ stock-nacos/src/main/java/com/tulingxueyuan/stock/controller/StockController.java
@@ -0,0 +1,21 @@
+package com.tulingxueyuan.stock.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.RequestWrapper;
+
+/**
+ * @Description:
+ * @author: 刘
+ * @date: 2023年02月06日 下午 4:23
+ */
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+    @RequestMapping("/reduct")
+    public String reduct(){
+        System.out.println("扣减成功");
+        return "Hello World 2";
+    }
+}
