2f9c8f5456f197bf057152ed64e3948fb396bf8d
yqliu <3471966565@qq.com>
2023-02-17 17:56:19 +0800

    544：修改

--- sentinel_demo/src/main/java/com/towery/sentinel/controller/HelloController.java
+++ sentinel_demo/src/main/java/com/towery/sentinel/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.towery.sentinel.controller;
 
 import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.EntryType;
 import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.Tracer;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.towery.sentinel.pojo.User;
@@ -62,12 +66,52 @@ public class HelloController {
         //设置受保护的资源的阈值
         flowRule.setCount(1);
         rules.add(flowRule);
+
+        FlowRule rule2 = new FlowRule();
+        rule2.setResource(USER_RESOURCE_NAME);
+        rule2.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        //设置受保护的资源的阈值
+        rule2.setCount(1);
+        rules.add(rule2);
+
         FlowRuleManager.loadRules(rules);
     }
-
+    @PostConstruct
+    public void initDegradeRule(){
+        List<DegradeRule> rules = new ArrayList<>();
+        DegradeRule rule = new DegradeRule();
+        rule.setResource(DEGRADE_RESOURCE_NAME);
+        //设置规则为异常数
+        rule.setGrade(RuleConstant.DEGRADE_GRADE_EXCEPTION_COUNT);
+        //设置异常数
+        rule.setCount(2);
+        //触发熔断最少请求数
+        rule.setMinRequestAmount(2);
+        //统计时长
+        rule.setStatIntervalMs(60*1000);
+        //熔断时长，秒
+        rule.setTimeWindow(10);
+        rules.add(rule);
+        DegradeRuleManager.loadRules(rules);
+    }
 
     @RequestMapping("/user")
+    @SentinelResource(value = USER_RESOURCE_NAME,blockHandler ="blockHandlerForGetUser" )
     public User getuser(String id){
             return new User("zhangsan");
     }
+    public User blockHandlerForGetUser(String id){
+        return new User("流控");
+    }
+
+    @RequestMapping("degrade")
+    @SentinelResource(value = DEGRADE_RESOURCE_NAME,entryType = EntryType.IN,blockHandler = "blockHandlerForFb")
+    public User degrade(){
+        throw new RuntimeException("异常");
+    }
+    public User blockHandlerForFb(String id,BlockException e){
+        return new User("熔断降级");
+    }
+
+
 }
